d1765e93e1327197db0b5796774bd8db73f9f4d1
stepanenko <serhiy.stepanenko@aiesec.net>
2020-05-21 22:35:41 +0300

    updated packages, added dynamic style and class

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,6 +4,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  // styleUrls: ['./app.component.css']
+  styleUrls: ['./server.component.css']
 })
-export class ServerComponent {}
+export class ServerComponent {
+  status = 'online';
+
+  getColor() {
+    return 'red';
+  }
+
+  getFontColor() {
+    return 'white';
+  }
+}
